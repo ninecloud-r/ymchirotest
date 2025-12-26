d0389a8deaaea62aa8aa68a124d17548e619afe8
itomakito <info@naikura.com>
2025-12-26 15:29:42 +0900

    フロントページの出力

--- wp-content/themes/tktk1/front-page.php
+++ wp-content/themes/tktk1/front-page.php
@@ -5,187 +5,173 @@ Template Name: front-page
 ?>
 <?php get_header(); ?>
 <?php $url = get_theme_file_uri(); ?>
+
 <div class="section concept-section">
-		<div class="container mid">
-			<div class="section-img">
-	<img src="<?php echo $url?>/view/images/top_img01.webp" />
-				</div>
-				<div class="description-area">
-					しっかりした施術で<br>
-背骨・筋肉・内臓・頭蓋骨を整えます
-					</div>
-			</div>
-			</div>
+    <div class="container mid">
+        <div class="section-img">
+            <img src="<?php echo $url; ?>/view/images/top_img01.webp" alt="しっかりした施術で整えます">
+        </div>
+        <div class="description-area">
+            しっかりした施術で<br>
+            背骨・筋肉・内臓・頭蓋骨を整えます
+        </div>
+    </div>
+</div>
+
 <div class="section story-section">
-<div class="container">
-<div class="contents-area">
-<div class="content-box">
-<div class="txt-box">
-<h3></h3>
-<p>すると・・・<br>
-
-脳と体がしっかり連携する状態になります<br>
-
-
-もし、非常事態（体調不良）になっても<br>
-
-
-脳と体の連携は保たれます</p>
-
+    <div class="container">
+        <div class="contents-area">
+            <div class="content-box">
+                <div class="txt-box">
+                    <p>すると・・・<br>
+                    脳と体がしっかり連携する状態になります<br>
+                    もし、非常事態（体調不良）になっても<br>
+                    脳と体の連携は保たれます</p>
+                </div>
+                <div class="img-box">
+                    <img src="<?php echo $url; ?>/view/images/top_img03.webp" alt="脳と体の連携">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<div class="img-box"><img src="<?php echo $url?>/view/images/top_img03.webp" /></div>
-</div>
-</div>
-</div>
-</div>
+
 <div class="section concept-section">
-		<div class="container mid">
-			<div class="section-img">
-	<img src="<?php echo $url?>/view/images/top_img04.webp" />
-				</div>
-			</div>
-			</div>
+    <div class="container mid">
+        <div class="section-img">
+            <img src="<?php echo $url; ?>/view/images/top_img04.webp" alt="">
+        </div>
+    </div>
+</div>
+
 <div class="section story-section">
-<div class="container">
-<div class="contents-area">
-<div class="content-box">
-<div class="txt-box">
-<h3></h3>
-<p>この状態を維持すると・・・<br>
-
-どのような体調不良でも<br>
-
-何種類も体調不良があっても</p>
-
-</div>
-<div class="img-box"><img src="<?php echo $url?>/view/images/top_img02.webp" /></div>
-</div>
-</div>
-</div>
+    <div class="container">
+        <div class="contents-area">
+            <div class="content-box">
+                <div class="txt-box">
+                    <p>この状態を維持すると・・・<br>
+                    どのような体調不良でも<br>
+                    何種類も体調不良があっても</p>
+                </div>
+                <div class="img-box">
+                    <img src="<?php echo $url; ?>/view/images/top_img02.webp" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="section concept-section">
-		<div class="container mid">
-			<div class="section-img">
-	<img src="<?php echo $url?>/view/images/top_img06.webp" />
-				</div>
-				<div class="description-area">
-					てくてくの施術は、体が勝手に不調の克服を<br>
-		
-していくように、仕向けていくためのものです。
-					</div>
-					<div class="section-img">
-	<img src="<?php echo $url?>/view/images/top_img05.webp" />
+    <div class="container mid">
+        <div class="section-img">
+            <img src="<?php echo $url; ?>/view/images/top_img06.webp" alt="">
+        </div>
+        <div class="description-area">
+            てくてくの施術は、体が勝手に不調の克服を<br>
+            していくように、仕向けていくためのものです。
+        </div>
+        <div class="section-img">
+            <img src="<?php echo $url; ?>/view/images/top_img05.webp" alt="">
+        </div>
+    </div>
 </div>
-			</div>
-			</div>
-
 
 <div class="section top-col-section col01">
-	<div class="content-box">
-		<div class="img-box">
-		<img src="<?php echo $url?>/view/images/top_img07.webp" />
-		</div>
-	</div>
+    <div class="content-box">
+        <div class="img-box">
+            <img src="<?php echo $url; ?>/view/images/top_img07.webp" alt="">
+        </div>
+    </div>
 </div>
 
 <div class="section top-col-section col02">
-	<div class="content-box col-box-wrap">
-		<div class="col-box">
-  <div class="img-box">
-    <a href="/voice-headache">
-      <img src="<?php echo $url?>/view/images/top_img08.webp" alt="頭痛薬がいらなくなった皆さま" />
-    </a>
-  </div>
-  <h4>頭痛薬がいらなくなった皆さま</h4>
-  <div class="txt-box">長年飲んでいた薬が必要なくなった方が多くいらっしゃいます！</div>
-  <div class="btn-box">
-    <a href="/voice-headache">写真をクリックして特設ページへ！</a>
-  </div>
-</div>
-<div class="col-box">
-  <div class="img-box">
-    <a href="#">
-      <img src="<?php echo $url?>/view/images/top_img09.webp" alt="頭痛薬がいらなくなった皆さま" />
-    </a>
-  </div>
-  <h4>不眠を解消された皆さま</h4>
-  <div class="txt-box">不眠やめまい、ふらつきなどが減ったというお声をよくいただきます！</div>
-  <div class="btn-box">
-    <a href="#">写真をクリックして特設ページへ！</a>
-  </div>
-</div>
-<div class="col-box">
-  <div class="img-box">
-    <a href="#">
-      <img src="<?php echo $url?>/view/images/top_img10.gif" alt="頭痛薬がいらなくなった皆さま" />
-    </a>
-  </div>
-  <h4>坐骨神経痛を解消された皆さま</h4>
-  <div class="txt-box">医療機関では痛み止め処方や様子見になってしまう状態でも原因を追究し回復を目指します！</div>
-  <div class="btn-box">
-    <a href="#">写真をクリックして特設ページへ！</a>
-  </div>
-</div>
-		
-		
-	</div>
+    <div class="content-box col-box-wrap">
+        <div class="col-box">
+            <div class="img-box">
+                <a href="<?php echo home_url('/voice-headache'); ?>">
+                    <img src="<?php echo $url; ?>/view/images/top_img08.webp" alt="頭痛薬がいらなくなった皆さま" />
+                </a>
+            </div>
+            <h4>頭痛薬がいらなくなった皆さま</h4>
+            <div class="txt-box">長年飲んでいた薬が必要なくなった方が多くいらっしゃいます！</div>
+            <div class="btn-box">
+                <a href="<?php echo home_url('/voice-headache'); ?>">写真をクリックして特設ページへ！</a>
+            </div>
+        </div>
+        <div class="col-box">
+            <div class="img-box">
+                <a href="<?php echo home_url('/voice-insomnia'); ?>">
+                    <img src="<?php echo $url; ?>/view/images/top_img09.webp" alt="不眠を解消された皆さま" />
+                </a>
+            </div>
+            <h4>不眠を解消された皆さま</h4>
+            <div class="txt-box">不眠やめまい、ふらつきなどが減ったというお声をよくいただきます！</div>
+            <div class="btn-box">
+                <a href="<?php echo home_url('/voice-insomnia'); ?>">写真をクリックして特設ページへ！</a>
+            </div>
+        </div>
+        <div class="col-box">
+            <div class="img-box">
+                <a href="<?php echo home_url('/voice-sciatica'); ?>">
+                    <img src="<?php echo $url; ?>/view/images/top_img10.gif" alt="坐骨神経痛を解消された皆さま" />
+                </a>
+            </div>
+            <h4>坐骨神経痛を解消された皆さま</h4>
+            <div class="txt-box">医療機関では痛み止め処方や様子見になってしまう状態でも原因を追究し回復を目指します！</div>
+            <div class="btn-box">
+                <a href="<?php echo home_url('/voice-sciatica'); ?>">写真をクリックして特設ページへ！</a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="section line-section">
-	<div class="container">
-		<div class="section-box">
-		<div class="section-title">
-			<i class="fa-brands fa-line"></i>てくてくLINE公式アカウント
-		</div>
-		<div class="text-box">
-			LINEからのご予約も承ります。
-		</div>
-		<div class="btn-box">
-			<a href="#">友達登録する</a>
-		</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="section-box">
+            <div class="section-title">
+                <i class="fa-brands fa-line"></i>てくてくLINE公式アカウント
+            </div>
+            <div class="text-box">
+                LINEからのご予約も承ります。
+            </div>
+            <div class="btn-box">
+                <a href="https://line.me/your-id" target="_blank" rel="noopener">友達登録する</a>
+            </div>
+        </div>
+    </div>
 </div>
-	
-<div class="section contact-section">
-	<div class="container">
-		<div class="btn-box">
-	<div class="section-title">ご予約・お問い合わせ</div>
-	<div class="text-box">長年の不調の根本解決をお手伝いします</div>
-	
-<div class="btn btn-forec">
 
-	<a href="./contact.php">ご予約・お問い合わせ</a></div>
-</div>
-	</div>
+<div class="section contact-section">
+    <div class="container">
+        <div class="btn-box">
+            <div class="section-title">ご予約・お問い合わせ</div>
+            <div class="text-box">長年の不調の根本解決をお手伝いします</div>
+            <div class="btn btn-forec">
+                <a href="<?php echo home_url('/contact'); ?>">ご予約・お問い合わせ</a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="section voice-section">
-	<div class="container">
-		<div class="section-title">
-			ご利用者様の声<br>
-			
-（ほかにも色々な不調に対応しております）
-		</div>
-		<div class="section-description">
-			長年の不調から解放されたご利用者様のお喜びの声が少しずつ増えてきました
-		</div>
-		<div class="col-box-wrap">
-			<div class="col-box">
-				<div class="img-box"><img src="<?php echo $url?>/view/images/top_voi01.webp" alt="" /></div>
-				<div class="text-box">肩こりからくる頭痛</div>
-			</div>
-			<div class="col-box">
-				<div class="img-box"><img src="<?php echo $url?>/view/images/top_voi02.webp" alt="" /></div>
-				<div class="text-box">肩こりからくる頭痛</div>
-			</div>
-			<div class="col-box">
-				<div class="img-box"><img src="<?php echo $url?>/view/images/top_voi03.webp" alt="" /></div>
-				<div class="text-box">肩こりからくる頭痛</div>
-			</div>
-		</div>
-	</div>
+    <div class="container">
+        <div class="section-title">ご利用者様の声<br>（ほかにも色々な不調に対応しております）</div>
+        <div class="section-description">長年の不調から解放されたご利用者様のお喜びの声が少しずつ増えてきました</div>
+        <div class="col-box-wrap">
+            <div class="col-box">
+                <div class="img-box"><img src="<?php echo $url; ?>/view/images/top_voi01.webp" alt="" /></div>
+                <div class="text-box">肩こりからくる頭痛</div>
+            </div>
+            <div class="col-box">
+                <div class="img-box"><img src="<?php echo $url; ?>/view/images/top_voi02.webp" alt="" /></div>
+                <div class="text-box">肩こりからくる頭痛</div>
+            </div>
+            <div class="col-box">
+                <div class="img-box"><img src="<?php echo $url; ?>/view/images/top_voi03.webp" alt="" /></div>
+                <div class="text-box">肩こりからくる頭痛</div>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="section feature-section">
@@ -194,68 +180,35 @@ Template Name: front-page
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-regular fa-calendar-check"></i></div>
                 <div class="col-title">施術は2週間に1回</div>
-                <div class="text-box">
-                    本当に体を健康にすることができる技術があれば2週間に1度施術をすれば十分です。ほとんどの院では、効果が持続しないために2〜3日に1回、一週間に1回などの頻度ですが、当院の場合は頻繁に通っていただく必要はありません。
-                </div>
+                <div class="text-box">本当に体を健康にすることができる技術があれば2週間に1度施術をすれば十分です。</div>
             </div>
-
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-solid fa-couch"></i></div>
                 <div class="col-title">付き添いの方のための<br>ベンチあります</div>
-                <div class="text-box">
-                    施術の様子をご覧いただける位置に、付き添いの方のためのベンチを用意しております。どうぞご一緒にお越しくださいませ。
-                </div>
+                <div class="text-box">施術の様子をご覧いただける位置に、付き添いの方のためのベンチを用意しております。</div>
             </div>
-
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-solid fa-car"></i></div>
                 <div class="col-title">自動車での来院もOK</div>
-                <div class="text-box">
-                    施術院前に自動車を駐車できますので、ぜひご利用くださいませ。ぎっくり腰やひざ痛の方の送り迎えにもどうぞ。
-                </div>
+                <div class="text-box">施術院前に自動車を駐車できますので、ぜひご利用くださいませ。</div>
             </div>
-
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-solid fa-ban"></i></div>
                 <div class="col-title">物品を売りつけません</div>
-                <div class="text-box">
-                    施術院の中には皆さまに何か物を売りつけようとするところがありますが、当院はそういったことは一切いたしません。症状を改善していくにあたり、道具やサプリメントなどはまったく必要ありません。
-                </div>
+                <div class="text-box">症状を改善していくにあたり、道具やサプリメントなどはまったく必要ありません。</div>
             </div>
-
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-solid fa-shield-heart"></i></div>
                 <div class="col-title">安心、安全な施術です</div>
-                <div class="text-box">
-                    当院では急激に痛みを伴ったり、強い力を加えるような施術は行ないません。お子様から女性、ご年配の方まで安心して受けていただけます。実際に施術中に寝てしまわれる方もいます。機械を使うこともありません。
-                </div>
+                <div class="text-box">当院では急激に痛みを伴ったり、強い力を加えるような施術は行ないません。</div>
             </div>
-
             <div class="col-box">
                 <div class="benefit-box-icon"> <i class="fa-solid fa-handshake-slash"></i></div>
                 <div class="col-title">施術を強制しません</div>
-                <div class="text-box">
-                    お話しを伺った結果、当院での施術がふさわしくないと判断した場合には、他の施設や医療機関をお勧めする場合もあります。なんでもかんでも強制的に施術することはせず、皆さまの体調回復にとってベストな選択をしていくことをお約束します。
-                </div>
-            </div>			
+                <div class="text-box">皆さまの体調回復にとってベストな選択をしていくことをお約束します。</div>
+            </div>
         </div>
     </div>
-</div>
-
-<div class="section line-section">
-	<div class="container">
-		<div class="section-box">
-		<div class="section-title">
-			<i class="fa-brands fa-line"></i>てくてくLINE公式アカウント
-		</div>
-		<div class="text-box">
-			LINEからのご予約も承ります。
-		</div>
-		<div class="btn-box">
-			<a href="#">友達登録する</a>
-		</div>
-		</div>
-	</div>
 </div>
 
 <section class="section blog-section">
@@ -266,162 +219,43 @@ Template Name: front-page
     </div>
 
     <div class="blog-contents-wrap">
+      <?php
+      $args = array(
+          'post_type'      => 'post',
+          'posts_per_page' => 3,
+          'no_found_rows'  => true, // ページネーション不要な場合、高速化
+      );
+      $blog_query = new WP_Query($args);
+      if ($blog_query->have_posts()) : while ($blog_query->have_posts()) : $blog_query->the_post();
+      ?>
       <article class="blog-item">
-        <a href="#">
+        <a href="<?php the_permalink(); ?>">
           <div class="blog-img">
-            <img src="<?php echo $url?>/view/images/top_voi04.webp" alt="記事タイトル">
-            <span class="category-tag">コラム</span>
+            <?php if (has_post_thumbnail()) : ?>
+              <?php the_post_thumbnail('medium'); ?>
+            <?php else : ?>
+              <img src="<?php echo $url; ?>/view/images/top_voi04.webp" alt="">
+            <?php endif; ?>
+            <span class="category-tag"><?php $cat = get_the_category(); echo $cat[0]->name; ?></span>
           </div>
           <div class="blog-text">
-            <time class="date">2025.12.25</time>
-            <h4 class="post-title">頭痛を根本から改善するための日常の習慣について</h4>
-            <p class="post-excerpt">当院に多く寄せられる頭痛のお悩み。実は日々のちょっとした姿勢の癖が原因かもしれません...</p>
+            <time class="date"><?php echo get_the_date('Y.m.d'); ?></time>
+            <h4 class="post-title"><?php the_title(); ?></h4>
+            <p class="post-excerpt"><?php echo wp_trim_words(get_the_excerpt(), 40, '...'); ?></p>
           </div>
         </a>
       </article>
-
-      <article class="blog-item">
-        <a href="#">
-          <div class="blog-img">
-            <img src="<?php echo $url?>/view/images/top_voi04.webp" alt="記事タイトル">
-            <span class="category-tag news">お知らせ</span>
-          </div>
-          <div class="blog-text">
-            <time class="date">2025.12.20</time>
-            <h4 class="post-title">年末年始の営業についてのお知らせ</h4>
-            <p class="post-excerpt">本年度の営業は12月30日までとなります。新年は1月4日より...</p>
-          </div>
-        </a>
-      </article>
+      <?php endwhile; wp_reset_postdata(); endif; ?>
     </div>
 
     <div class="btn-box">
-      <a href="./blog" class="btn-readmore">記事一覧を見る</a>
+      <a href="<?php echo home_url('/blog'); ?>" class="btn-readmore">記事一覧を見る</a>
     </div>
   </div>
 </section>
 
 <div class="ggmap">
-	<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6496.61419078715!2d139.446396!3d35.496684!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018568b586c2ed9%3A0x465d7bc95eea82d8!2z5aSn5ZKM5Y2X5p6X6ZaT44Kr44Kk44Ot44OX44Op44Kv44OG44Kj44OD44Kv5pW05L2T6ZmiIOOBpuOBj-OBpuOBjw!5e0!3m2!1sja!2sjp!4v1766646206422!5m2!1sja!2sjp" width="100%" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-<!--固定ページ取得のためのコード-->
-<?php   $url = get_theme_file_uri(); ?>
-<?php if ( is_home() || is_front_page() ) : ?>
-<?php
-$page_id = 20;//ページIDを指定
-$page = get_post($page_id, 'OBJECT', 'raw'); //指定のページIDから情報を取得
-$page_include = apply_filters( 'the_content',$page->post_content); //ページの本文をフィルターフックで整形
-echo $page_include; //出力
-?>
-<?php endif; ?>
-
-		
-<?php
-// 1. 新しいクエリのための引数を定義
-$args = array(
-    'post_type' => 'post',
-    'post_status' => 'publish',
-    'category__in' => 10,
-    'posts_per_page' => 4,
-    'order' => 'DESC'
-);
-
-// 2. カスタムクエリオブジェクトを作成し、実行
-$custom_query = new WP_Query($args); // グローバル変数を汚染しない方法
-
-// 3. カスタムクエリを使ってループを開始
-if($custom_query->have_posts()){
-    while ($custom_query->have_posts()) {
-        $custom_query->the_post(); // the_post() ではなく、クエリオブジェクトのメソッドを使用
-?>
-        
-<a href="<?php the_permalink(); ?>"></a>
-<?php if ( has_post_thumbnail() ): ?><!-- if文による条件分岐 アイキャッチが有る時-->
-<?php the_post_thumbnail( 'thumbnail' ); ?>            
-<?php else: ?><!-- アイキャッチが無い時-->            
-<?php 
-$image = get_field('lp_main_img');
-$size = 'full'; // (thumbnail, medium, large, full or custom size)
-?>
-    <?php  if( $image ):?>
-        <?php echo wp_get_attachment_image( $image, $size );?>
-    <?php else: ?>
-        <img src="<?php echo first_image(); ?>" alt="<?php the_title(); ?>">
-    <?php endif; ?>
-<?php endif; ?>
-
-<a href="<?php echo get_permalink(); ?>" >
-<?php the_title(); ?>
-</a>        	
-            </li>
-<?php
-    }
-    
-    // 4. カスタムクエリ終了後、メインクエリの投稿データを復元
-    wp_reset_postdata(); // ★ wp_reset_query() ではなく、wp_reset_postdata() を使用
-}
-?>
-
-<?php
-// クエリの定義 (安定したカスタムクエリ方式)
-$args = array(
-    'post_type'      => 'post',
-    'post_status'    => 'publish',
-    'category__in'   => 73,
-    'posts_per_page' => 6,
-    'order'          => 'DESC',
-    'orderby'        => 'date',
-    'ignore_sticky_posts' => 1
-);
-
-$custom_query = new WP_Query($args);
-
-if ($custom_query->have_posts()) {
-    while ($custom_query->have_posts()) {
-        $custom_query->the_post();
-        
-        // ★ 1. first_image()の結果を変数に格納し、サイズチェックを行う準備
-        $image_url = null;
-        $image_valid = false;
-        $min_width = 100; // 最小幅を100pxに設定（アイコンや絵文字を無視）
-
-        if (!has_post_thumbnail()) {
-            // アイキャッチがない場合のみ、first_imageを試す
-            $image_url = first_image();
-            
-            if ($image_url) {
-                // 画像サイズチェックを実行
-                $image_size = @getimagesize($image_url);
-                
-                if ($image_size && $image_size[0] >= $min_width) {
-                    $image_valid = true;
-                }
-            }
-        }
-?>
-    <li>
-        
-                    
-                    <?php if (has_post_thumbnail()): ?>
-                        <?php the_post_thumbnail('thumbnail'); // サムネイルサイズを指定 ?>
-                        
-                    <?php elseif ($image_valid): ?>
-                        <img src="<?php echo esc_url($image_url); ?>" alt="<?php the_title_attribute(); ?>">
-                        
-                    <?php else: ?>
-                        <img src="<?php echo esc_url($url); ?>/view/images/top_img09.jpg" alt="<?php the_title_attribute(); ?>" />
-                        
-                    <?php endif; ?>
-                    
-                
-                <p class="time-box"><?php the_time('Y/m/d'); ?></p>
-           
-            <?php the_title(); ?>
-        
-    </li>
-<?php
-    }
-    wp_reset_postdata(); 
-}
-?>
+    <iframe src="https://www.google.com/maps/embed?pb=..." width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+</div>
 
 <?php get_footer(); ?>
