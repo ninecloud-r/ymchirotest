fb594e06ce4d627275e87528c973fa7879949337
itomakito <info@naikura.com>
2025-12-29 12:40:24 +0900

    アーカイブページ、ジェミニ

--- wp-content/themes/tktk1/archive.php
+++ wp-content/themes/tktk1/archive.php
@@ -1,77 +1,59 @@
-<?php
-/*
-Template Name: seminar
-*/
-?>
 <?php get_header(); ?>
-			
+
 <div class="section blog-section">
-							<div class="container lg">
-								<div class="section-title">
-									<h2 class="en-title">News & Blog</h2>
-									<p class="jp-title">
-										お知らせとブログ#
-										</p>
-									</div>
-									
-                                
-										<div class="contents-area">
-											<div class="contents-wrap">
-												<ul class="blog-contents-wrap">
-                                                 <?php
-$wp_query = new WP_Query();
-$args = array(
-'post_type' => 'post',
-'post_status' => 'publish',
-'category__in' => 226,
-'posts_per_page' => 12,
-'order' => 'DESC'
-);
-$wp_query->query($args);
-if($wp_query->have_posts()){
-?>
-                        
-<?php
-while (have_posts()) {
-the_post();
-?>   
-                                                    
-													<li>
-														<a href="<?php echo get_permalink(); ?>" class="archive-column">
-														<div class="blog-icon">
-                                                            <div class="img-box">
-                                                            <?php if(first_image()): ?>
-  <img src="<?php echo first_image(); ?>" alt="<?php the_title(); ?>">
-                                                            <?php else: ?>
+    <div class="container lg">
+        <div class="section-title">
+            <h2 class="en-title">
+                <?php
+                if (is_category()) {
+                    single_cat_title();
+                } elseif (is_archive()) {
+                    echo 'Archives';
+                }
+                ?>
+            </h2>
+            <p class="jp-title">
+                <?php 
+                if (is_category('voice')) {
+                    echo 'お客様の声';
+                } else {
+                    echo 'お知らせとブログ';
+                }
+                ?>
+            </p>
+        </div>
 
-															<img src="<?php echo $url?>/view/images/top_img09.jpg" />
-                                                            <?php endif; ?>
-                                                                </div>
-															<p class="time-box">
-																<?php the_time('Y/m/d'); ?>
-																</p>
-															</div>
-														<h4><?php the_title(); ?></h4>
-														</a>
+        <div class="contents-area">
+            <div class="contents-wrap">
+                <ul class="blog-contents-wrap">
+                    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+                        <li>
+                            <a href="<?php the_permalink(); ?>" class="archive-column">
+                                <div class="blog-icon">
+                                    <div class="img-box">
+                                        <?php if (has_post_thumbnail()) : ?>
+                                            <?php the_post_thumbnail('medium'); ?>
+                                        <?php else : ?>
+                                            <img src="<?php echo get_template_directory_uri(); ?>/view/images/top_img09.jpg" alt="no image" />
+                                        <?php endif; ?>
+                                    </div>
+                                    <p class="time-box"><?php the_time('Y/m/d'); ?></p>
+                                </div>
+                                <h4><?php the_title(); ?></h4>
+                                <?php if (!is_category('information')) : // お知らせ以外なら抜粋を表示する場合 ?>
+                                    <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 40, '...'); ?></p>
+                                <?php endif; ?>
+                            </a>
+                        </li>
+                    <?php endwhile; endif; ?>
+                </ul>
 
-														</li>
-                                                    
+                <div class="pagination">
+                    <?php the_posts_pagination(); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
-<?php
-}
-wp_reset_query();
-}
-?>
-														
-													</ul>
-												</div>
-												</div>
-												
-
-
-											</div>
-
-								</div>
-		
-</div><!-- container -->
 <?php get_footer(); ?>
