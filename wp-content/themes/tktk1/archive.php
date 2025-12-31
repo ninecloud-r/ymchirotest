5020fc2d0c881c88be4ed2dbd8fad5dd1f3005ac
itomakito <info@naikura.com>
2025-12-31 12:16:44 +0900

    修正修正と

--- wp-content/themes/tktk1/archive.php
+++ wp-content/themes/tktk1/archive.php
@@ -24,59 +24,64 @@
             <div class="contents-wrap">
                 <ul class="blog-contents-wrap">
                     <?php if (have_posts()) : while (have_posts()) : the_post(); 
-                        $current_pt = get_post_type();
+    $current_pt = get_post_type();
+    // データの取得（voice以外でも、もし入力があれば表示されるようになります）
+    $v_name   = get_post_meta(get_the_ID(), 'name', true);
+    $v_age    = get_post_meta(get_the_ID(), 'age', true);
+    $v_letter = get_post_meta(get_the_ID(), 'letter', true);
+?>
+    <li>
+        <a href="<?php the_permalink(); ?>" class="archive-column">
+            <div class="blog-icon">
+                <div class="img-box">
+                    <?php if (has_post_thumbnail()) : ?>
+                        <?php the_post_thumbnail('full'); ?>
+                    <?php else : ?>
+                        <img src="<?php echo get_template_directory_uri(); ?>/view/images/top_img09.jpg" alt="no image" />
+                    <?php endif; ?>
+                </div>
+                
+                <p class="category-box">
+                    <?php
+                    // タクソノミーまたはカテゴリーを表示
+                    if ( $current_pt === 'voice' ) {
+                        $terms = get_the_terms(get_the_ID(), 'parts');
+                        echo ($terms && !is_wp_error($terms)) ? esc_html($terms[0]->name) : 'お客様の声';
+                    } else {
+                        $cats = get_the_category();
+                        echo ($cats) ? esc_html($cats[0]->name) : 'ブログ';
+                    }
                     ?>
-                        <li>
-                            <a href="<?php the_permalink(); ?>" class="archive-column">
-                                <div class="blog-icon">
-                                    <div class="img-box">
-                                        <?php if (has_post_thumbnail()) : ?>
-                                            <?php the_post_thumbnail('full'); ?>
-                                        <?php else : ?>
-                                            <img src="<?php echo get_template_directory_uri(); ?>/view/images/top_img09.jpg" alt="no image" />
-                                        <?php endif; ?>
-                                    </div>
-                                    
-                                    <p class="category-box">
-                                        <?php
-                                        if ( $current_pt === 'voice' ) {
-                                            $terms = get_the_terms(get_the_ID(), 'parts');
-                                            echo ($terms && !is_wp_error($terms)) ? esc_html($terms[0]->name) : 'お客様の声';
-                                        } else {
-                                            $cats = get_the_category();
-                                            echo ($cats) ? esc_html($cats[0]->name) : 'ブログ';
-                                        }
-                                        ?>
-                                    </p>
-                                </div>
+                </p>
+            </div>
 
-                                <div class="blog-text">
-                                    <h4><?php the_title(); ?></h4>
+            <div class="blog-text">
+                <h4><?php the_title(); ?></h4>
 
-                                    <?php if ( $current_pt === 'voice' ) : ?>
-                                        <?php 
-                                            $v_name = get_post_meta(get_the_ID(), 'name', true);
-                                            $v_age  = get_post_meta(get_the_ID(), 'age', true);
-                                            $v_letter = get_post_meta(get_the_ID(), 'letter', true);
-                                            
-                                            if($v_name || $v_age): 
-                                        ?>
-                                            <p class="voice-meta"><strong><?php echo esc_html($v_name); ?>様</strong>（<?php echo esc_html($v_age); ?>代）</p>
-                                        <?php endif; ?>
+                <?php if($v_name || $v_age): ?>
+                    <p class="voice-meta">
+                        <strong><?php echo esc_html($v_name); ?>様</strong>
+                        <?php if($v_age): ?>（<?php echo esc_html($v_age); ?>代）<?php endif; ?>
+                    </p>
+                <?php endif; ?>
 
-                                        <?php if ($v_letter) : ?>
-                                            <p class="excerpt"><?php echo wp_trim_words(esc_html($v_letter), 45, '...'); ?></p>
-                                        <?php endif; ?>
+                <p class="excerpt">
+                    <?php 
+                    if ( $current_pt === 'voice' && $v_letter ) {
+                        // voiceでお手紙データがある場合
+                        echo wp_trim_words(esc_html($v_letter), 45, '...');
+                    } else {
+                        // それ以外、またはお手紙データがない場合
+                        echo wp_trim_words(get_the_excerpt(), 40, '...');
+                    }
+                    ?>
+                </p>
 
-                                    <?php else : ?>
-                                        <p class="excerpt"><?php echo wp_trim_words(get_the_excerpt(), 40, '...'); ?></p>
-                                    <?php endif; ?>
-
-                                    <p class="time-box"><?php the_time('Y/m/d'); ?></p>
-                                </div>
-                            </a>
-                        </li>
-                    <?php endwhile; endif; ?>
+                <p class="time-box"><?php the_time('Y/m/d'); ?></p>
+            </div>
+        </a>
+    </li>
+<?php endwhile; endif; ?>
                 </ul>
 
                 <div class="pagination">
