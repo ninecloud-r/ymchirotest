69274a82809a8aa4bf6a45b32fa14a82bd6518cf
itomakito <info@naikura.com>
2026-01-02 10:45:04 +0900

    ページ上部調整

--- wp-content/themes/tktk1/page.php
+++ wp-content/themes/tktk1/page.php
@@ -1,6 +1,8 @@
 <?php get_header(); ?>
+<div class="container mid">
 			<?php while ( have_posts() ) : the_post(); ?>
 						<?php the_content(); ?>
 			<?php endwhile; ?>
+		</div>
 			</div>
 <?php get_footer(); ?>
