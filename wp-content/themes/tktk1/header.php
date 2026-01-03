52956044397a825d6be79830d7fb1081e15db535
itomakito <info@naikura.com>
2026-01-03 10:14:06 +0900

    色上書きできるかな

--- wp-content/themes/tktk1/header.php
+++ wp-content/themes/tktk1/header.php
@@ -19,11 +19,11 @@
 <?php wp_title( '--', true, 'right' ); ?>
 <?php bloginfo( 'name' ); ?>
 </title>
-<link media="screen" type="text/css" href="<?php echo $url?>/view/css/style.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
 <script src="<?php echo $url?>/view/js/common.js" type="text/javascript"></script>
+<link media="screen" type="text/css" href="<?php echo $url?>/view/css/style.css" rel="stylesheet">
 <!-- Google Tag Manager -->
 <!-- End Google Tag Manager -->
 <?php wp_head(); ?>
