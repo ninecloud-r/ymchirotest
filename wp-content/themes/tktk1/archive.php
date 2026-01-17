a934e0c37db04a587d56e15506678bf5230a7bed
itomakito <info@naikura.com>
2026-01-17 12:22:12 +0900

    これでどうでしょう。

--- wp-content/themes/tktk1/archive.php
+++ wp-content/themes/tktk1/archive.php
@@ -116,36 +116,7 @@
                 </div>
             </div>
         </div>
-        <div class="banner-contents">
-            <div class="banner-box">
-                <div class="banner">
-                <a href="#">
-                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
-                    <p class="title-box">施術ブログ・コラム</p></a>
-                </div>
-            </div>
-            <div class="banner-box">
-                <div class="banner">
-                <a href="#">
-                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
-                    <p class="title-box">施術方法</p></a>
-                </div>
-            </div>
-            <div class="banner-box">
-                <div class="banner">
-                <a href="#">
-                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
-                    <p class="title-box">回復された方の声</p></a>
-                </div>
-            </div>
-            <div class="banner-box">
-                <div class="banner">
-                <a href="#">
-                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
-                    <p class="title-box">症状別の事例</p></a>
-                </div>
-            </div>
-        </div>
+        <?php get_template_part('banner'); ?>
     </div>
 </main>
 
---
+++ wp-content/themes/tktk1/banner.php
@@ -0,0 +1,31 @@
+
+        <div class="banner-contents">
+            <div class="banner-box">
+                <div class="banner">
+                <a href="#">
+                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
+                    <p class="title-box">施術ブログ・コラム</p></a>
+                </div>
+            </div>
+            <div class="banner-box">
+                <div class="banner">
+                <a href="#">
+                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
+                    <p class="title-box">施術方法</p></a>
+                </div>
+            </div>
+            <div class="banner-box">
+                <div class="banner">
+                <a href="#">
+                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
+                    <p class="title-box">回復された方の声</p></a>
+                </div>
+            </div>
+            <div class="banner-box">
+                <div class="banner">
+                <a href="#">
+                    <p class="img-box"><img src="<?php echo $url; ?>/view/images/7004.jpg" alt="" /></p>
+                    <p class="title-box">症状別の事例</p></a>
+                </div>
+            </div>
+        </div>
