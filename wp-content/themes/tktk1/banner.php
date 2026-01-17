42256157c22be5c37f1a92163c6c9025cbb5d38f
itomakito <info@naikura.com>
2026-01-17 12:52:00 +0900

    ACF導入

--- wp-content/themes/tktk1/banner.php
+++ wp-content/themes/tktk1/banner.php
@@ -29,3 +29,29 @@
                 </div>
             </div>
         </div>
+
+
+        -----
+
+        <div class="banner-contents">
+    <?php for ( $i = 1; $i <= 4; $i++ ) : 
+        // フィールド名（banner_1_img, banner_2_img...）を動的に生成
+        $img   = get_field( "banner_{$i}_img" );
+        $title = get_field( "banner_{$i}_title" );
+        $link  = get_field( "banner_{$i}_url" );
+        
+        // 全ての項目が入っている場合のみ表示
+        if ( $img && $title && $link ) :
+    ?>
+    <div class="banner-box">
+        <div class="banner">
+            <a href="<?php echo esc_url($link); ?>">
+                <p class="img-box">
+                    <img src="<?php echo esc_url($img); ?>" alt="<?php echo esc_attr($title); ?>" />
+                </p>
+                <p class="title-box"><?php echo esc_html($title); ?></p>
+            </a>
+        </div>
+    </div>
+    <?php endif; endfor; ?>
+</div>
