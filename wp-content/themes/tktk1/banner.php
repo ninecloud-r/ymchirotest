e3d0ab718fa1435d8d6632d1ff5708678707328c
itomakito <info@naikura.com>
2026-01-17 13:06:53 +0900

    画像呼び出しACF

--- wp-content/themes/tktk1/banner.php
+++ wp-content/themes/tktk1/banner.php
@@ -33,25 +33,37 @@
 
         -----
 
-        <div class="banner-contents">
-    <?php for ( $i = 1; $i <= 4; $i++ ) : 
-        // フィールド名（banner_1_img, banner_2_img...）を動的に生成
-        $img   = get_field( "banner_{$i}_img" );
-        $title = get_field( "banner_{$i}_title" );
-        $link  = get_field( "banner_{$i}_url" );
+       <div class="banner-contents">
+    <?php 
+    // バナーを管理しているページのIDを固定
+    $banner_page_id = 6980; 
+
+    for ( $i = 1; $i <= 4; $i++ ) : 
+        // 第2引数に $banner_page_id を入れて、どのページからも 6980 のデータを呼ぶようにする
+        $img_data = get_field( "banner_{$i}_img", $banner_page_id );
+        $title    = get_field( "banner_{$i}_title", $banner_page_id );
+        $link     = get_field( "banner_{$i}_url", $banner_page_id );
         
-        // 全ての項目が入っている場合のみ表示
-        if ( $img && $title && $link ) :
+        // 画像URLを取得（配列で返ってきている場合にも対応）
+        $img_url = "";
+        if ( is_array($img_data) ) {
+            $img_url = $img_data['url']; // 配列（デフォルト）の場合
+        } elseif ( is_string($img_data) ) {
+            $img_url = $img_data; // URL（設定変更済み）の場合
+        }
+
+        if ( $img_url && $title && $link ) :
     ?>
     <div class="banner-box">
         <div class="banner">
             <a href="<?php echo esc_url($link); ?>">
                 <p class="img-box">
-                    <img src="<?php echo esc_url($img); ?>" alt="<?php echo esc_attr($title); ?>" />
+                    <img src="<?php echo esc_url($img_url); ?>" alt="<?php echo esc_attr($title); ?>" />
                 </p>
                 <p class="title-box"><?php echo esc_html($title); ?></p>
             </a>
         </div>
     </div>
     <?php endif; endfor; ?>
+</div>
 </div>
